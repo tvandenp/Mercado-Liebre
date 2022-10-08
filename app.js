c9864079df178f77759ee1764b8c272f3309f1ce
Tvandenp <tomas.vanden@gmail.com>
2022-10-08 11:12:27 -0300

    Carga Mercado Liebre 1.0v

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log("Se está utilizando el puerto " + port);
+});
+
+const publicPath = path.resolve(__dirname, "./public");
+
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/login.html"));
+});
